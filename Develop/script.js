b3d8b2c1d8840152419bc3cf72a1b714bc949a88
Leo Liu <aleoliu96@gmail.com>
2023-04-07 00:22:23 -0400

    First add

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,11 @@
 // Assignment code here
+var characterLength = 8;
+var choice = [];
+
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numbers = ["1","2","3","4","5","6","7","8","9","0"];
+var symbols = ["~","!","@","#","$","%","^","&","*","?","-","_"];
 
 
 // Get references to the #generate element
@@ -13,5 +20,36 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+
+}
+
+
+
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function getPrompts() {
+  characterLength = parseInt(prompt("How many charcters do you want your password to be? (8 - 12 characters)")); 
+
+  if(isNaN(characterLength) || characterLength < 8 || characterLength > 12) {
+    alert("Character length has to be a number, 8-12 digits. Please try again.");
+    return false;
+  }
+
+  if (confirm("Would you like lowercase letters in your password?")) {
+    choice = choice.concat(lowerCase);
+  }
+  if (confirm("Would you like uppercase letters in your password?")) {
+    choice = choice.concat(upperCase);
+  }
+  if (confirm("Would you like numbers in your password?")) {
+    choice = choice.concat(numbers);
+  }
+  if (confirm("Would you like symbols in your password?")) {
+    choice = choice.concat(symbols);
+  }
+}
