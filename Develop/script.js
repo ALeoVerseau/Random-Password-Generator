924a4def753603c11c50b678697449a3e2c7705f
Leo Liu <aleoliu96@gmail.com>
2023-04-07 22:30:44 -0400

    Final add

--- Develop/script.js
+++ Develop/script.js
@@ -2,10 +2,11 @@
 var characterLength = 8;
 var choice = [];
 
-var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var numbers = ["1","2","3","4","5","6","7","8","9","0"];
-var symbols = ["~","!","@","#","$","%","^","&","*","?","-","_"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var symbols = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "?", "-", "_"];
+
 
 
 // Get references to the #generate element
@@ -16,59 +17,50 @@ function writePassword() {
   var correct = getPrompts(); // either true or flase
   var passwordText = document.querySelector("#password");
 
-  if (correct) { 
+  if (correct) {
     var password = generatePassword();
-    
-  
+
+
     passwordText.value = password;
   } else {
     passwordTest.value = "";
 
   }
-  
-
-  
-
 }
 
 function generatePassword() {
   var password = "";
   for (var i = 0; i < characterLength; i++) {
     var randomIndex = Math.floor(Math.random() * choice.length);
-    password = password + choice[randomIndex]; 
-  
+    password = password + choice[randomIndex];
+
   }
   return password;
 }
-
-
-
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 function getPrompts() {
   choice = [];
-  characterLength = parseInt(prompt("How many charcters do you want your password to be? (8 - 128 characters)")); 
+  characterLength = parseInt(prompt("How many charcters do you want your password to be? (8 - 128 characters)"));
 
-  if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
-    alert("Character length has to be a number, 8-12 digits. Please try again.");
+  if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+    alert("Password length has to be a number, 8-12 digits. Please try again.");
     return false;
   }
 
   if (confirm("Would you like lowercase letters in your password?")) {
-    choice = choice.concat(lowerCase);
+    choice = choice.concat(lowerCase); // concat combines two or more arrays: in this case it combines all lower case letters
   }
   if (confirm("Would you like uppercase letters in your password?")) {
-    choice = choice.concat(upperCase);
+    choice = choice.concat(upperCase); // combines more than 1 uppercase letters
   }
   if (confirm("Would you like numbers in your password?")) {
-    choice = choice.concat(numbers);
+    choice = choice.concat(numbers); // combines more than 1 digit
   }
-  if (confirm("Would you like symbols in your password?")) {
-    choice = choice.concat(symbols);
+  if (confirm("Would you like symbols in your password?")) { //confirm prompts the question to be yes or no
+    choice = choice.concat(symbols); // combines more than 1 symbol
   }
 
   return true;
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Password Generator Starter Code
+The Random Password Generator challenge
+Challenge #3 for the GT bootcamp 
+
+# Description
+
+This challenge is a showcase on how we build a random password generator within javascript. 
+
+## Installation
+
+N/A
+
+## Usage
+
+To generate a random password 
+
+## Credits
+
+Leo Liu
+
+## License
+
+Please refer to the LICENSE in the repo.
+
+## Screenshot 
+![Screenshot](https://user-images.githubusercontent.com/125929076/230699314-cb962f7a-df78-4f61-8dbd-4dc63d674b8c.png)
